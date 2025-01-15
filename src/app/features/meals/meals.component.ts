37feeab31e2aa14ce9c5fdca355df2983ace3f35
rangelovv <rangelovvjobs@gmail.com>
2025-01-15 13:38:47 +0200

    created the meals feature view

--- src/app/features/meals/meals.component.ts
+++ src/app/features/meals/meals.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MealCardComponent } from "./components/meal-card/meal-card.component";
+import { meals } from "./models/meals.models";
 
 @Component({
-  selector: 'app-meals',
-  imports: [],
-  templateUrl: './meals.component.html'
+  selector: "app-meals",
+  imports: [MealCardComponent],
+  templateUrl: "./meals.component.html",
 })
 export class MealsComponent {
-
+  meals = meals;
 }
