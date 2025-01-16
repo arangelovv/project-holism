903a5146d2a657ee671e7e6d4d7dd4fcd0f02d3c
rangelovv <rangelovvjobs@gmail.com>
2025-01-16 12:33:09 +0200

    minor changes resolving around the toolbar

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,24 +1,28 @@
 import { Component, HostListener } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { NavigationEnd, RouterOutlet } from "@angular/router";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 import { Router } from "@angular/router";
-import { NgIf } from "@angular/common";
 
 @Component({
   selector: "app-core",
-  imports: [RouterOutlet, SidebarComponent, ToolbarComponent, NgIf],
+  imports: [RouterOutlet, SidebarComponent, ToolbarComponent],
   templateUrl: "./core.component.html",
 })
 export class CoreComponent {
   screenWidth: number = window.innerWidth;
-  isHomeRoute = false;
+  showToolbar = false;
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.router.events.subscribe(() => {
-      this.isHomeRoute = this.router.url === "/home";
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.router.url;
+        this.showToolbar =
+          currentRoute.includes("/meals") ||
+          currentRoute.includes("/exercises");
+      }
     });
   }
 
