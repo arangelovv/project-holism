5e2196369656b79edad0f8f9262e3382e1ba61b1
rangelovv <rangelovvjobs@gmail.com>
2025-01-18 14:13:02 +0200

    setup angular and firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,40 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
+import { enviroment } from "./enviroments/enviroment.dev";
+import { getApp, initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { connectAuthEmulator, getAuth, provideAuth } from "@angular/fire/auth";
+import {
+  connectFirestoreEmulator,
+  Firestore,
+  getFirestore,
+  initializeFirestore,
+  provideFirestore,
+} from "@angular/fire/firestore";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(enviroment.firebase)),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (enviroment.useEmulators) {
+        connectAuthEmulator(auth, "http://localhost:9099");
+      }
+      return auth;
+    }),
+    provideFirestore(() => {
+      let firestore: Firestore;
+      if (enviroment.useEmulators) {
+        firestore = initializeFirestore(getApp(), {
+          experimentalForceLongPolling: true,
+        });
+        connectFirestoreEmulator(firestore, "localhost", 8088);
+      } else {
+        firestore = getFirestore();
+      }
+      return firestore;
+    }),
+  ],
 };
